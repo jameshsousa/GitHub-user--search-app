faf8bb2343a6be8262870c91d915d290cf645382
James Holanda <jameshsousa@gmail.com>
2023-04-22 22:44:50 -0300

    fix the javascript code

--- src/script.js
+++ src/script.js
@@ -1,14 +1,15 @@
 document.getElementById("light").addEventListener("click", function() {
     document.getElementById('dark').style.display = "block";
     document.getElementById('light').style.display = "none";
-    document.getElementsByTagName('body').style.background = "white";
+    document.getElementById('test').style.background = "white";
+    document.getElementById('test').style.color = "#141d2f";
 });
 
 document.getElementById("dark").addEventListener("click", function() {
     document.getElementById('light').style.display = "block";
     document.getElementById('dark').style.display = "none";
-    document.getElementsByTagName('body').style.background = "#141d2f";
-    document.getElementsByTagName('body').style.color = "white";
+    document.getElementById('test').style.background = "#141d2f"; 
+    document.getElementById('test').style.color = "white";    
 });
 
 
