276f3e8b5778eddb008c6a3fd588d4beb6980135
James Holanda <jameshsousa@gmail.com>
2023-04-24 23:22:30 -0300

    fix some error of the javascript code

--- src/script.js
+++ src/script.js
@@ -6,7 +6,7 @@ document.getElementById("light").addEventListener("click", function() {
     document.getElementById('nav').style.background = "white";
     document.getElementById('main').style.background = "white";
     document.getElementById('nav').style.border = "0.1rem solid #141d2f";
-    document.getElementById('main').style.border = "0.1rem solid #141d2f";
+    document.getElementById('main').style.border = "0.1rem solid #141d2f";    
       
 });
 
@@ -17,7 +17,47 @@ document.getElementById("dark").addEventListener("click", function() {
     document.getElementById('test').style.color = "white";
     document.getElementById('nav').style.background = "#1E2A47";
     document.getElementById('main').style.background = "#1E2A47";    
+    
 });
+
+async function searchUser() {
+    const searchInput = document.getElementById("searchInput");
+    const username = searchInput.value;
+
+    if (!username) {
+        alert("Digite um nome de usuário válido.");
+        return;
+    }
+
+    const response = await fetch(`https://api.github.com/users/${username}`);
+    const data = await response.json();
+
+    if (data.message === "Not Found") {
+        showError();
+    } else {
+        showUserProfile(data);
+    }
+}
+
+function showError() {
+    const userProfile = document.getElementById("userProfile");
+    const error = document.getElementById("error");
+
+    userProfile.classList.add("hidden");
+    error.classList.remove("hidden");
+}
+
+function showUserProfile(data) {
+
+
+    document.getElementById("repoCount").textContent = data.public_repos;
+    document.getElementById("followerCount").textContent = data.followers;
+    document.getElementById("followingCount").textContent = data.following;
+    document.getElementById("joinDate").textContent = new Date(data.created_at).toLocaleDateString();
+    document.getElementById("location").textContent = data.location || "Não disponível";
+    document.getElementById("twitter").textContent = data.twitter_username ? `@${data.twitter_username}` : "Não disponível";
+}
+
 
 
 
